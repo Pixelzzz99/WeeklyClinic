9fcbdcd8d383de2b593c9ebd8a10cb5d45f8f244
Pixelzzz <sherzik99@gmail.com>
2023-12-10 20:07:18 +0500

    added doctor registration

--- main.cpp
+++ main.cpp
@@ -2,116 +2,18 @@
 #include <iostream>
 #include <string>
 #include <memory>
+
 #include "patient.repository.hpp"
 #include "patient.service.hpp"
 #include "patient.controller.hpp"
 
-// sqlite3 *db;
-
-// void initDB()
-// {
-//   int rc = sqlite3_open("./clinic.db", &db);
-
-//   if (rc)
-//   {
-//     std::cout << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
-//     exit(1);
-//   }
-
-//   const std::string createPatientsTable = "CREATE TABLE IF NOT EXISTS patients (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, address TEXT, phone TEXT, medical_historey TEXT)";
-
-//   const std::string createAppointmentsTable = "CREATE TABLE IF NOT EXISTS appointments (id INTEGER PRIMARY KEY, patient_id INTEGER, date TEXT, time INTEGER, doctor_id INTEGER)";
-
-//   const std::string createDoctorsTable = "CREATE TABLE IF NOT EXISTS doctors (id INTEGER PRIMARY KEY, name TEXT, specialization TEXT, address TEXT, phone TEXT)";
-
-//   sqlite3_exec(db, createPatientsTable.c_str(), 0, 0, 0);
-//   sqlite3_exec(db, createDoctorsTable.c_str(), 0, 0, 0);
-//   sqlite3_exec(db, createAppointmentsTable.c_str(), 0, 0, 0);
-// }
-
-// void registerPatient(const crow::request &req, crow::response &res)
-// {
-//   try
-//   {
-//     auto json = crow::json::load(req.body);
-//     res.add_header("Content-Type", "application/json");
-
-//     if (!json)
-//     {
-//       res.code = 400;
-//       throw std::runtime_error("Invalid JSON");
-//     }
-
-//     const std::string name = json["name"].s();
-//     const int age = json["age"].i();
-//     std::cout << "Age: " << age << std::endl;
-//     const std::string gender = json["gender"].s();
-//     const std::string address = json["address"].s();
-//     const std::string phone = json["phone"].s();
-//     const std::string medical_history = json["medical_history"].s();
-//     std::cout << "HERE" << std::endl;
-
-//     if (name.empty() || age < 0 || gender.empty() || address.empty() || phone.empty() || medical_history.empty())
-//     {
-//       res.code = 400;
-//       throw std::runtime_error("Required fields are empty: name, age, gender, address, phone, medical_historey");
-//     }
-
-//     sqlite3_stmt *stmt;
-//     const std::string insertPatientQuery = "INSERT INTO patients (name, age, gender, address, phone, medical_historey) VALUES (?,?,?,?,?,?)";
-//     int rc = sqlite3_prepare_v2(db, insertPatientQuery.c_str(), -1, &stmt, 0);
-//     if (rc != SQLITE_OK)
-//     {
-//       std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << std::endl;
-//       res.code = 500;
-//       throw std::runtime_error("Internal server error");
-//     }
-
-//     sqlite3_bind_text(stmt, 1, name.c_str(), name.size(), SQLITE_STATIC);
-//     sqlite3_bind_int(stmt, 2, age);
-//     sqlite3_bind_text(stmt, 3, gender.c_str(), gender.size(), SQLITE_STATIC);
-//     sqlite3_bind_text(stmt, 4, address.c_str(), address.size(), SQLITE_STATIC);
-//     sqlite3_bind_text(stmt, 5, phone.c_str(), phone.size(), SQLITE_STATIC);
-//     sqlite3_bind_text(stmt, 6, medical_history.c_str(), medical_history.size(), SQLITE_STATIC);
-
-//     rc = sqlite3_step(stmt);
-
-//     if (rc != SQLITE_DONE)
-//     {
-//       std::cerr << "Failed to insert patient: " << sqlite3_errmsg(db) << std::endl;
-//       res.code = 500;
-//       throw std::runtime_error("Internal server error");
-//     }
-
-//     sqlite3_finalize(stmt);
-//     crow::json::wvalue result = crow::json::wvalue::object();
-//     res.code = 200;
-//     result["id"] = sqlite3_last_insert_rowid(db);
-//     result["message"] = "Patient registered successfully";
-//     res.write(result.dump());
-//     res.end();
-//   }
-//   catch (const std::exception &e)
-//   {
-//     std::cerr << e.what() << '\n';
-//     crow::json::wvalue error = crow::json::wvalue::object();
-//     error["error"] = e.what();
-//     res.write(error.dump());
-//     res.end();
-//   }
-// }
+#include "doctor.repository.hpp"
+#include "doctor.service.hpp"
+#include "doctor.controller.hpp"
 
 int main()
 {
-  // initDB();
-  crow::SimpleApp app; // define your crow application
-
-  // // define your endpoint at the root directory
-  // CROW_ROUTE(app, "/register_patient").methods(crow::HTTPMethod::POST)(registerPatient);
-
-  // // set the port, set the app to run on multiple threads, and run the app
-
-  // sqlite3_close(db);
+  crow::SimpleApp app;
 
   SqliteORM *orm = new SqliteORM();
   orm->connect("clinic.db");
@@ -119,66 +21,11 @@ int main()
   std::shared_ptr<PatientService> patientService = std::make_shared<PatientService>(patients);
   std::shared_ptr<PatientController> patientController = std::make_shared<PatientController>(app, patientService);
 
+  std::shared_ptr<DoctorRepository> doctors = std::make_shared<DoctorRepository>(orm);
+  std::shared_ptr<DoctorService> doctorService = std::make_shared<DoctorService>(doctors);
+  std::shared_ptr<DoctorController> doctorController = std::make_shared<DoctorController>(app, doctorService);
+
   app.port(18080).multithreaded().run();
-  // PatientController patientController(app);
-  // DoctorController doctorController(app);
-  // // auto patientsTable = new PatientRepository(orm);
-
-  // auto patients = orm->createEntity("patients", {{"id", columnTypes::INT, true},
-  //                                                {"name", columnTypes::TEXT},
-  //                                                {"age", columnTypes::INT},
-  //                                                {"gender", columnTypes::TEXT},
-  //                                                {"address", columnTypes::TEXT},
-  //                                                {"phone", columnTypes::TEXT},
-  //                                                {"medical_history", columnTypes::TEXT},
-  //                                                {"count_of_visits", columnTypes::INT}});
-
-  // auto doctors = orm->createEntity("doctors", {
-  //                                                 {"id", columnTypes::INT, true},
-  //                                                 {"name", columnTypes::TEXT},
-  //                                                 {"age", columnTypes::INT},
-  //                                                 {"gender", columnTypes::TEXT},
-  //                                                 {"speciality", columnTypes::TEXT},
-  //                                             });
-
-  // for (int i = 18; i < 25; i++)
-  // {
-  //   patients->insert({{1, "Maria" + std::to_string(i)},
-  //                     {2, std::to_string(i)},
-  //                     {3, "Female"},
-  //                     {4, "2323 Main St"},
-  //                     {5, "1234556789"},
-  //                     {6, "Kidney Failure"},
-  //                     {7, std::to_string(23)}});
-  // }
-  // auto result = patients->select({"gender = 'Female'", "age > 20"});
-
-  // for (auto patient : result)
-  // {
-  //   std::cout << patient["id"] << " \t "
-  //             << patient["name"] << " \t "
-  //             << patient["age"] << " \t "
-  //             << patient["gender"] << " \t "
-  //             << patient["address"] << " \t "
-  //             << patient["phone"] << " \t "
-  //             << patient["medical_history"] << std::endl;
-  // }
-
-  // auto resultUpdate = patients->update(17, {{"medical_history", "Healthy"}, {"count_of_visits", "1"}, {"address", "234 Main St"}});
-  // patients->delete_(24);
-  // result = patients->select({"age < 20", "gender = 'Male'"});
-
-  // for (auto patient : result)
-  // {
-  //   std::cout << patient["id"] << " \t "
-  //             << patient["name"] << " \t "
-  //             << patient["age"] << " \t "
-  //             << patient["gender"] << " \t "
-  //             << patient["address"] << " \t "
-  //             << patient["phone"] << " \t "
-  //             << patient["medical_history"] << " \t "
-  //             << patient["count_of_visits"] << std::endl;
-  // }
 
   return 0;
 }
--- src/doctors/doctor.service.cpp
+++ src/doctors/doctor.service.cpp
@@ -3,6 +3,17 @@
 
 DoctorService::DoctorService(std::shared_ptr<DoctorRepository> repository) : doctorsRepository(repository) {}
 
-void DoctorService::registerDoctor()
+std::string DoctorService::registerDoctor(std::string name, std::string specialization, std::string address, std::string phone)
 {
+    if (name.empty() || specialization.empty() || address.empty() || phone.empty())
+    {
+        return "";
+    }
+
+    this->doctorsRepository->insert({{1, name},
+                                     {2, specialization},
+                                     {3, address},
+                                     {4, phone}});
+
+    return name;
 }
